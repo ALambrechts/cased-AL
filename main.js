2735a183400b99ffc69a10221de435ba057ac4b0
Arthur Lambrechts <arthur.lambrechts.al@gmail.com>
2022-04-22 16:51:32 +0200

    quickslider

--- main.js
+++ main.js
@@ -8,3 +8,39 @@ function changeProductImage(event) {
 }
 
 $productsSlider.addEventListener('click', changeProductImage);
+
+// slider for steps
+
+function enableStepSlider() {
+  let $SliderContainer = document.querySelector('.product-process__container');
+  let $nextBtn = document.getElementById('step-slider-next');
+  let $prevBtn = document.getElementById('step-slider-prev');
+
+  let currentSlide = 0;
+  let slideCount = $SliderContainer.children.length;
+
+  function nextBtnClick() {
+    if (currentSlide === slideCount - 2) {
+      return;
+    }
+
+    currentSlide++;
+    $SliderContainer.style.left = `${-currentSlide * 50}%`;
+  }
+
+  function prevBtnClick() {
+    if (currentSlide === 0) {
+      return;
+    }
+
+    currentSlide--;
+    $SliderContainer.style.left = `${-currentSlide * 50}%`;
+  }
+
+  $nextBtn.addEventListener('click', nextBtnClick);
+  $prevBtn.addEventListener('click', prevBtnClick);
+}
+
+window.addEventListener('load', function () {
+  enableStepSlider();
+});
